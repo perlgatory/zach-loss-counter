cfbdf79c81dbcb0178ec4a628e323bac7b2118ec
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-11-13 13:37:05 -0600

    Update index view to display all existing bets in a table

--- bets/urls.py
+++ bets/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /bets/
     path('', views.index, name='index'),
+    # ex: /bets/1/
+    path('<int:bet_id>/', views.detail, name='detail'),
 ]
--- bets/views.py
+++ bets/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Bet
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the bets index.")
+    bets = Bet.objects.order_by('created_at')
+    template = loader.get_template('bets/index.html')
+    context = {
+        'bets': bets,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, bet_id):
+    return HttpResponse("You're looking at the details of bet {}".format(bet_id))
